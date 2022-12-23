f12635741f9d7254acc76e33249e89567a5f2c5f
Patrik <79817868+IamPatrikexe@users.noreply.github.com>
2022-12-23 01:19:57 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,5 @@
 config = {
-    password = "mypassword", -- Max of 30 characters
-    message = "Enter server password to continue", -- Message will show above password field
-    failMessage = "Incorrect password" -- Failed message will show after wrong password is entered
+    password = "IDEAJELSZÓT", -- Maximum 30 karakter
+    message = "A folytatáshoz adja meg a szerver jelszavát", -- A jelszó mező felett üzenet jelenik meg
+    failMessage = "Helytelen jelszó" -- Sikertelen üzenet jelenik meg a rossz jelszó megadása után
 }
